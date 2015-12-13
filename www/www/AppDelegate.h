016b6023e70d47cc34fe747d1f662629cdd4e3d2
hqc <hqc@hqc.local>
2015-12-13 16:59:22 +0800

    初始化

---
+++ www/www/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  www
+//
+//  Created by hqc on 15/12/13.
+//  Copyright © 2015年 hqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ www/www/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  www
+//
+//  Created by hqc on 15/12/13.
+//  Copyright © 2015年 hqc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
